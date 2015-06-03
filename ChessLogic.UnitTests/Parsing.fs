d0985f599b05f1b66998565a67bf67621c8c1f74
ilya <marhoily@homrmail.com>
2015-06-03 12:32:20 +0300

    troubleshoot some more

--- ChessLogic.UnitTests/Parsing.fs
+++ ChessLogic.UnitTests/Parsing.fs
@@ -2,13 +2,15 @@
 
 open FsUnit.Xunit
 open Parsing
+open System.Text
 
 let shouldEqual (a:string) (b:string) = 
     if a <> b then
         printfn "expected:"
         printfn "%A" (a.Replace(" ", "·"))
+        printfn "%A" (Encoding.ASCII.GetBytes(a))
         printfn "but was:"
-        printfn "%A" (b.Replace(" ", "·"))
+        printfn "%A" (Encoding.ASCII.GetBytes(b))
         failwith "should equal failed"
 let ParseToStringShouldMatch toString parse input = 
     toString (unwrap (parse input)) |> should equal input
