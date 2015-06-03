5973afa55f1d3e27509b0ceb05b93313375f6eee
ilya <marhoily@homrmail.com>
2015-06-03 12:41:15 +0300

    fix build

--- ChessLogic.UnitTests/Parsing.fs
+++ ChessLogic.UnitTests/Parsing.fs
@@ -4,13 +4,25 @@ open FsUnit.Xunit
 open Parsing
 open System.Text
 
-let shouldEqual (a:string) (b:string) = 
+let shouldEqual (actual:string) (expected:string) = 
+    let a = actual.Replace("\r\n", "\n")
+    let b = expected.Replace("\r\n", "\n")
     if a <> b then
         printfn "expected:"
-        printfn "%A" (a.Replace(" ", "·"))
-        printfn "%A" (Encoding.ASCII.GetBytes(a))
+        printfn "%A" a//(a.Replace(" ", "·"))
         printfn "but was:"
-        printfn "%A" (Encoding.ASCII.GetBytes(b))
+        printfn "%A" b//(Encoding.ASCII.GetBytes(b))
+//        if a.Length <> b.Length then
+//            printfn "letngths do not match. expected: %A, but was %A" a.Length b.Length
+//        else
+//            let mutable position = 0
+//            let cmp x y =
+//                if x <> y then
+//                    printfn "symbols at index %A do not match: '%A' '%A'" position x y
+//                position <- position + 1
+//            Seq.iter2 cmp a b
+//                
+
         failwith "should equal failed"
 let ParseToStringShouldMatch toString parse input = 
     toString (unwrap (parse input)) |> should equal input
