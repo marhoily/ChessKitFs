2433996301ba5a8763acb1ca8b283ffca750191e
ilya <marhoily@homrmail.com>
2015-06-03 12:25:24 +0300

    troubleshoot tests

--- ChessLogic.UnitTests/Parsing.fs
+++ ChessLogic.UnitTests/Parsing.fs
@@ -3,7 +3,14 @@
 open FsUnit.Xunit
 open Parsing
 
+let shouldEqual (a:string) (b:string) = 
+    if a <> b then
+        printfn "expected:"
+        printfn "%A" (a.Replace(" ", "·"))
+        printfn "but was:"
+        printfn "%A" (b.Replace(" ", "·"))
+        failwith "should equal failed"
 let ParseToStringShouldMatch toString parse input = 
     toString (unwrap (parse input)) |> should equal input
 let ErrorMessageShouldMatch parse (input : string) (msg : string) = 
-    getErrorMessage (parse input) |> should equal msg
+    getErrorMessage (parse input) |> shouldEqual msg
