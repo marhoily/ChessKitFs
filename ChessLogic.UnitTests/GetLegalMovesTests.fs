d95cacf2857d9609252b9e7bd53ef169b008d620
ilya <marhoily@homrmail.com>
2015-06-05 14:43:00 +0300

    + 2 pawn tests

--- ChessLogic.UnitTests/GetLegalMovesTests.fs
+++ ChessLogic.UnitTests/GetLegalMovesTests.fs
@@ -6,18 +6,27 @@ open Parsing
 open MoveLegalityChecker
 open FenParser
 open CoordinateNotation
-open FenPrinter
 open Definitions
+open Dump
 
 let check from position = 
-    position
-    |> ParseFen
-    |> unwrap
-    |> GetLegalMoves.FromSquare (_c from)
-    |> List.map CoordinateToString
+    let p = position |> ParseFen |> unwrap
+    printf "%s" (Print p)
+    p
+    |> GetLegalMoves.FromSquare(_c from)
+    |> List.map (fun m -> 
+           match m.Move with
+           | UsualMove(_, t) -> CoordinateToString t
+           | PromotionMove({Vector = (_, t)}) -> CoordinateToString t)
+
+[<Fact>]
+let ``white pawn on h6``() = 
+    "8/8/7P/8/8/8/8/8 w - - 0 1"
+    |> check "h6"
+    |> should equal [ "h7" ]
 
 [<Fact>]
 let ``white pawn on h7``() = 
     "8/7P/8/8/8/8/8/8 w - - 0 1"
     |> check "h7"
-    |> should equal ["g8"]           
+    |> should equal [ "h8" ]
--- ChessLogic/GetLegalMoves.fs
+++ ChessLogic/GetLegalMoves.fs
@@ -2,6 +2,20 @@
 
 open CoordinateNotation
 open Definitions
+open MoveLegalityChecker
 
-let FromSquare from position : Coordinate list =
-  []
+let FromSquare from position =
+    let fP = position |> PieceAt from 
+    let fromSquare = toX88 from
+    let at p c = p |> PieceAt (fromX88 c)
+    match fP with
+    | Some(White, Pawn) -> 
+        if snd from = 1 then
+            [position |> ValidateMove (
+                PromotionMove({ 
+                    Vector = (from, (from |> toX88) - 16 |> fromX88);
+                    PromoteTo = Queen}))]
+        else
+            [position |> ValidateMove (UsualMove(from, (from |> toX88) - 16 |> fromX88))]
+    | _ -> []
+    |> List.filter (fun m -> m.Hint.Errors |> List.length = 0)
