76f2b382e856d43295cd548aeb8a85339255fb7d
ilya <marhoily@homrmail.com>
2015-06-05 23:32:07 +0300

    add test

--- ChessLogic.UnitTests/SanTests.fs
+++ ChessLogic.UnitTests/SanTests.fs
@@ -23,3 +23,10 @@ let check move expectedSan position =
 let ``O-O``() = 
     "r3kb1r/pppb1pp1/8/7p/3PPBn1/2N5/PPP3PP/R2QK2R w KQkq - 0 14" 
     |> check "e1-g1" "O-O"
+
+[<Fact>]
+let ``O-O-O``() = 
+    "r3kn1r/pp4pp/1q2b3/n2pPpP1/2pP4/2P1BB2/PP4QP/RN3RK1 b kq - 0 16" 
+    |> check "e8-c8" "O-O-O"
+
+    
