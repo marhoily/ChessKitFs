67798e0a1e143d6f212a273f83428ec391900306
ilya <marhoily@homrmail.com>
2015-06-05 16:29:32 +0300

    fix index out of range

--- ChessLogic/GetLegalMoves.fs
+++ ChessLogic/GetLegalMoves.fs
@@ -4,24 +4,26 @@ open CoordinateNotation
 open Definitions
 open MoveLegalityChecker
 
-let FromSquare from position =
+let FromSquare from position = 
     let rank7 = 1
     let rank2 = 6
-    let m i = (from, (from |> toX88) + i |> fromX88)
-    let p i = PromotionMove({ Vector = m i; PromoteTo = Queen})
-    let u i = UsualMove(m i)
-    let g v i = position |> ValidateMove (v i)
-    let gen v il = il |> List.map (g v)
-    match position |> PieceAt from  with
+    
+    let p i = 
+        PromotionMove({ Vector = i
+                        PromoteTo = Queen })
+    
+    let u i = UsualMove(i)
+    
+    let gen v = 
+        List.map (fun i -> (from |> toX88) + i)
+        >> List.filter (fun x -> (x &&& 0x88) = 0)
+        >> List.map (fun t -> position |> ValidateMove(v (from, t |> fromX88)))
+    match position |> PieceAt from with
     | Some(White, Pawn) -> 
-        if snd from = rank7 then
-            gen p [-16; -15; -17]
-        else
-            gen u [-16; -32; -15; -17]
+        if snd from = rank7 then gen p [ -16; -15; -17 ]
+        else gen u [ -16; -32; -15; -17 ]
     | Some(Black, Pawn) -> 
-        if snd from = rank2 then
-            gen p [+16; +15; +17]
-        else
-            gen u [+16; +32; +15; +17]
+        if snd from = rank2 then gen p [ +16; +15; +17 ]
+        else gen u [ +16; +32; +15; +17 ]
     | _ -> []
     |> List.filter (fun m -> m.Hint.Errors |> List.isEmpty)
