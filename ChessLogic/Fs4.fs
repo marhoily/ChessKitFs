1bf756a8671da7963e4675f83f55b4c7ec3fde65
ilya <marhoily@homrmail.com>
2015-06-06 15:36:54 +0300

    refactor

--- ChessLogic/Fs4.fs
+++ ChessLogic/Fs4.fs
@@ -1,19 +1,11 @@
-﻿//namespace Fs4
+﻿module MyList
 
-module MyList
+open System.Collections.Generic
 
-    open System.Collections.Generic
+// Can replace with List.contains in F# 4.0
+let contains item = List.exists (fun i -> i = item)
 
-    // Can replace with List.contains in F# 4.0
-    let contains item = List.exists (fun i -> i = item)
-    // Can replace with List.except in F# 4.0
-    let except (itemsToExclude: 'T list) list =
-        match box itemsToExclude with
-        | null -> nullArg "itemsToExclude"
-        | _ -> ()
-
-        match list with
-        | [] -> list
-        | _ ->
-            let cached = new HashSet<'T>(itemsToExclude, HashIdentity.Structural)
-            list |> List.filter cached.Add
+// Can replace with List.except in F# 4.0
+let except (itemsToExclude : 'T list) list = 
+    let cached = new HashSet<'T>(itemsToExclude, HashIdentity.Structural)
+    list |> List.filter cached.Add
