7d74862594db4fb747afcae952f0fe19d7de7546
ilya <marhoily@homrmail.com>
2015-06-06 14:18:43 +0300

    refactor ToSanString

--- ChessLogic/San.fs
+++ ChessLogic/San.fs
@@ -17,11 +17,10 @@ let ToSanString (board : Position) (move : LegalMove) : string =
         | King -> 'K'
     
     let decompose m = (m.Start, m.End, m.PromoteTo)
-    
     let piece = move.Piece
     let sb = new StringBuilder(6)
     let castling = move.Castling
-    let (f, t, p) = (move.Start, move.End, move.PromoteTo)
+    //let (f, t, p) = (move.Start, move.End, move.PromoteTo)
     let file x = fileToStirng (x |> fst)
     let rank x = rankToString (x |> snd)
     let fileAndRank = CoordinateToString
@@ -36,12 +35,13 @@ let ToSanString (board : Position) (move : LegalMove) : string =
         lazy ([ for move2 in board |> GetLegalMoves.All do
                     let (f2, t2, _) = decompose move2
                     let at x = board |> PieceAt x
-                    if f <> f2 && t = t2 && at f = at f2 then yield f2 ])
+                    if move.Start <> f2 && move.End = t2 
+                       && at move.Start = at f2 then yield f2 ])
     
     let ambiguous() = not (disambiguationList.Value |> List.isEmpty)
     let unique fn = 
         disambiguationList.Value 
-        |> List.forall (fun f2 -> (f |> fn) <> (f2 |> fn))
+        |> List.forall (fun f2 -> (move.Start |> fn) <> (f2 |> fn))
     let shortCastling = castling = Some(WK) || castling = Some(BK)
     let longCastling = castling = Some(WQ) || castling = Some(BQ)
     // Actual algorithm
@@ -49,18 +49,18 @@ let ToSanString (board : Position) (move : LegalMove) : string =
     else if longCastling then append "O-O-O"
     else 
         if piece = Pawn then 
-            if capture then append (file f)
+            if capture then append (file move.Start)
         else 
             appendc (piece |> typeToString)
             if ambiguous() then 
-                if unique fst then append (file f)
-                else if unique snd then append (rank f)
-                else append (fileAndRank f)
+                if unique fst then append (file move.Start)
+                else if unique snd then append (rank move.Start)
+                else append (fileAndRank move.Start)
         if capture then appendc 'x'
-        append (fileAndRank t)
+        append (fileAndRank move.End)
     if promotion then 
         appendc '='
-        appendc (p |> typeToString)
+        appendc (move.PromoteTo |> typeToString)
     if check then appendc '+'
     else 
         if mate then appendc '#'
