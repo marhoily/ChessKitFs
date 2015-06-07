87df8cbacfa0ebac0f86bf8f20689c0f9d3245d8
ilya <marhoily@homrmail.com>
2015-06-07 21:47:34 +0300

    refactor

--- ChessLogic/MoveLegalityChecker.fs
+++ ChessLogic/MoveLegalityChecker.fs
@@ -29,54 +29,6 @@ type Error =
     | DoesNotMoveThisWay
     | CastleFromCheck
 
-[<Literal>]
-let A1 = 112
-
-[<Literal>]
-let B1 = 113
-
-[<Literal>]
-let C1 = 114
-
-[<Literal>]
-let D1 = 115
-
-[<Literal>]
-let E1 = 116
-
-[<Literal>]
-let F1 = 117
-
-[<Literal>]
-let G1 = 118
-
-[<Literal>]
-let H1 = 119
-
-[<Literal>]
-let A8 = 0
-
-[<Literal>]
-let B8 = 1
-
-[<Literal>]
-let C8 = 2
-
-[<Literal>]
-let D8 = 3
-
-[<Literal>]
-let E8 = 4
-
-[<Literal>]
-let F8 = 5
-
-[<Literal>]
-let G8 = 6
-
-[<Literal>]
-let H8 = 7
-
 type LegalMove = 
     { Start : Coordinate
       End : Coordinate
