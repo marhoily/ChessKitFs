93f3fa7cccdceeffe6f5089e6d85430fb992d3c0
ilya <marhoily@homrmail.com>
2015-06-14 18:14:15 +0300

    refactor

--- ChessLogic.UnitTests/AddObservationsTests.fs
+++ ChessLogic.UnitTests/AddObservationsTests.fs
@@ -24,23 +24,25 @@ let checkObservations position move expectedObservations =
     |> observationsToString
     |> should equal expectedObservations
 
-let play start moves = 
-    let rec iterate m p = 
-        match m with
-        | [] -> p
-        | head :: tail -> 
-            match p |> FromSanString head with
-            | LegalSan(legal, _) -> iterate tail (CoreToPosition legal)
-            | x -> 
-                printfn "%s" (ToFen p)
-                printfn "%s" (Dump.Print p)
-                printfn "%s" head
-                failwithf "%A" x
-    //    start
-    //    |> ParseFen
-    //    |> unwrap
-    //    |> iterate moves
-    iterate start moves
+let rec playFrom m p = 
+    match m with
+    | [] -> p
+    | head :: tail -> 
+        match p |> FromSanString head with
+        | LegalSan(legal, _) -> playFrom tail (CoreToPosition legal)
+        | x -> 
+            printfn "%s" (ToFen p)
+            printfn "%s" (Dump.Print p)
+            printfn "%s" head
+            failwithf "%A" x
+
+let playFen start moves = 
+    start
+    |> ParseFen
+    |> unwrap
+    |> playFrom moves
+
+let play moves = StartingPosition |> playFrom moves
 
 [<Fact>]
 let ``Gives check``() = 
@@ -57,7 +59,7 @@ let ``Play should work``() =
             [ "e4"; "e5"; "Nf3"; "Nc6"; "d4"; "exd4"; "Nxd4"; "Nf6"; "Nc3"; 
               "Bb4"; "Nxc6"; "bxc6"; "Bd3"; "d5"; "exd5"; "cxd5"; "O-O"; "O-O"; 
               "Bg5"; "Be6"; "Qf3"; "Be7"; "Rfe1"; "h6"; "Bxh6"; "gxh6"; "Rxe6"; 
-              "fxe6"; "Qg3+"; "Kh8"; "Qg6" ] StartingPosition
+              "fxe6"; "Qg3+"; "Kh8"; "Qg6" ]
     ToFen res 
     |> should equal "r2q1r1k/p1p1b3/4pnQp/3p4/8/2NB4/PPP2PPP/R5K1 b - - 3 16"
 
@@ -68,10 +70,10 @@ let ``PositionCore structural equality works``() =
     let c2 = (ParseFen fen |> unwrap).Core
     c1 |> should equal c2
     c1.GetHashCode() |> should equal (c2.GetHashCode())
-    [c1; c2]
+    [ c1; c2 ]
     |> Seq.countBy id
     |> List.ofSeq
-    |> should equal [(c1, 2)]
+    |> should equal [ (c1, 2) ]
 
 [<Fact>]
 let ``Draw by threefold repetition``() = 
@@ -81,7 +83,7 @@ let ``Draw by threefold repetition``() =
               "Bb4"; "Nxc6"; "bxc6"; "Bd3"; "d5"; "exd5"; "cxd5"; "O-O"; "O-O"; 
               "Bg5"; "Be6"; "Qf3"; "Be7"; "Rfe1"; "h6"; "Bxh6"; "gxh6"; "Rxe6"; 
               "fxe6"; "Qg3+"; "Kh8"; "Qg6"; "Qe8"; "Qxh6+"; "Kg8"; "Qg5+"; "Kh8"; 
-              "Qh6+"; "Kg8"; "Qg5+"; "Kh8"; "Qh6+" ] StartingPosition
+              "Qh6+"; "Kg8"; "Qg5+"; "Kh8"; "Qh6+" ]
     let strings = res.Observations |> List.map toString
     let actual = String.concat ", " strings
     actual |> should equal "Repetition, Check"
