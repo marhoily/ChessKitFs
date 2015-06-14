e29877fe5f7d508198652a5665e48b378fa5f625
ilya <marhoily@homrmail.com>
2015-06-14 11:53:43 +0300

    refactor

--- ChessLogic/CoordinateNotation.fs
+++ ChessLogic/CoordinateNotation.fs
@@ -8,18 +8,20 @@ let ToIndex = function
     | (file, rank) -> rank * 8 + file
 let PieceAt coordinate position = position.Core.Placement.[ToIndex coordinate]
 
-
-
 let ToCoordinateNotation (m:Move) = m.AsString
 
 let private coordinate = 
-    pipe2 (anyOf "abcdefgh") pint32 
-        (fun file rank -> (LetterToFileNoCheck file, 8 - rank))
+    let parseFile (p : char) : File = int (p) - int ('a')
+    let parseRank c = 8 - (int c - int '0')
+    let file = anyOf "abcdefgh" |>> parseFile
+    let rank = anyOf "12345678" |>> parseRank
+    file .>>. rank
+
 let ParseCoordinate str = wrap (run coordinate str)
 let _c = ParseCoordinate >> unwrap
 
 let ParseCoordinateNotation str = 
-    let f = coordinate .>> ((pchar '-') <|> (pchar 'x'))
+    let f = coordinate .>> (pchar '-' <|> pchar 'x')
     let hint = anyOf "NBRQK" |>> parsePromotionHint
     let p = opt ((pchar '=') >>. hint) 
     let notation = pipe3 f coordinate p (Move.Create)
