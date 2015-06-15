2325d7d964fb6d86552fbf041b1967bf3438ab5f
ilya <marhoily@homrmail.com>
2015-06-15 22:52:22 +0300

    refactor

--- ChessLogic/FenPrinter.fs
+++ ChessLogic/FenPrinter.fs
@@ -5,60 +5,61 @@ open System
 open System.Text
 
 let ToFen p = 
-    let castlingHintToString = 
-        function 
-        | WQ -> 'Q'
-        | WK -> 'K'
-        | BQ -> 'q'
-        | BK -> 'k'
-    let colorAsString = 
-        function
+    let color = 
+        match p.Core.ActiveColor with
         | White -> "w"
         | Black -> "b"
     
-    let printOutCastling c = 
+    let castling = 
         let result = 
-            c
-            |> List.map castlingHintToString
+            p.Core.CastlingAvailability
+            |> List.map (function 
+                   | WQ -> 'Q'
+                   | WK -> 'K'
+                   | BQ -> 'q'
+                   | BK -> 'k')
             |> List.toArray
             // This can be just "System.String" in F# 4.0
             |> (fun arr -> (String(arr)))
         if result = "" then "-"
         else result
     
-    let printOutEnPassant c e = 
-        match e with
+    let enPassant = 
+        match p.Core.EnPassant with
         | Some(file) -> 
-            (fileToStirng file) + (if c = Black then "3"
+            (fileToStirng file) + (if p.Core.ActiveColor = Black then "3"
                                    else "6")
         | None -> "-"
     
     let sb = new StringBuilder()
+    let appendc (c : char) = sb.Append(c) |> ignore
+    let appends (s : string) = sb.Append(s) |> ignore
+    let appendi (i : int) = sb.Append(i) |> ignore
     let mutable skip = 0
     let mutable file = 0
     for square in p.Core.Placement do
         if file = 8 then 
-            sb.Append('/') |> ignore
+            appendc '/'
             file <- 0
         file <- file + 1
         match square with
-        | Some(p) -> 
+        | Some p -> 
             if skip > 0 then 
-                sb.Append(skip) |> ignore
+                appendi skip
                 skip <- 0
-            sb.Append(pieceToChar p) |> ignore
+            appendc (pieceToChar p)
         | None -> skip <- skip + 1
         if file = 8 && skip > 0 then 
-            sb.Append(skip) |> ignore
+            appendi skip
             skip <- 0
-    sb.Append(' ') |> ignore
-    sb.Append(colorAsString p.Core.ActiveColor) |> ignore
-    sb.Append(' ') |> ignore
-    sb.Append(printOutCastling p.Core.CastlingAvailability) |> ignore
-    sb.Append(' ') |> ignore
-    sb.Append(printOutEnPassant p.Core.ActiveColor p.Core.EnPassant) |> ignore
-    sb.Append(' ') |> ignore
-    sb.Append(p.HalfMoveClock) |> ignore
-    sb.Append(' ') |> ignore
-    sb.Append(p.FullMoveNumber) |> ignore
+    appendc ' '
+    appends color
+    appendc ' '
+    appends castling
+    appendc ' '
+    appends enPassant
+    appendc ' '
+    appendi p.HalfMoveClock
+    appendc ' '
+    appendi p.FullMoveNumber
     string sb
