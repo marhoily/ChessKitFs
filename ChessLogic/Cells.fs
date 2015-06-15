09e87f7d2dbab89611052e456148eb4901de8f7a
ilya <marhoily@homrmail.com>
2015-06-15 18:54:28 +0300

    fix build

--- ChessLogic/Cells.fs
+++ ChessLogic/Cells.fs
@@ -2,49 +2,49 @@
 module Cells
 
 [<Literal>]
-let internal A1 = 112
+let A1 = 112
 
 [<Literal>]
-let internal B1 = 113
+let B1 = 113
 
 [<Literal>]
-let internal C1 = 114
+let C1 = 114
 
 [<Literal>]
-let internal D1 = 115
+let D1 = 115
 
 [<Literal>]
-let internal E1 = 116
+let E1 = 116
 
 [<Literal>]
-let internal F1 = 117
+let F1 = 117
 
 [<Literal>]
-let internal G1 = 118
+let G1 = 118
 
 [<Literal>]
-let internal H1 = 119
+let H1 = 119
 
 [<Literal>]
-let internal A8 = 0
+let A8 = 0
 
 [<Literal>]
-let internal B8 = 1
+let B8 = 1
 
 [<Literal>]
-let internal C8 = 2
+let C8 = 2
 
 [<Literal>]
-let internal D8 = 3
+let D8 = 3
 
 [<Literal>]
-let internal E8 = 4
+let E8 = 4
 
 [<Literal>]
-let internal F8 = 5
+let F8 = 5
 
 [<Literal>]
-let internal G8 = 6
+let G8 = 6
 
 [<Literal>]
-let internal H8 = 7
+let H8 = 7
