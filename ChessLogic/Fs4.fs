e33bd570eba0cfaf5d0c0b3564da9a14afb53ec7
ilya <marhoily@homrmail.com>
2015-06-15 18:29:30 +0300

    hide methods

--- ChessLogic/Fs4.fs
+++ ChessLogic/Fs4.fs
@@ -3,9 +3,9 @@
 open System.Collections.Generic
 
 // Can replace with List.contains in F# 4.0
-let contains item = List.exists (fun i -> i = item)
+let internal contains item = List.exists (fun i -> i = item)
 
 // Can replace with List.except in F# 4.0
-let except (itemsToExclude : 'T list) list = 
+let internal except (itemsToExclude : 'T list) list = 
     let cached = new HashSet<'T>(itemsToExclude, HashIdentity.Structural)
     list |> List.filter cached.Add
