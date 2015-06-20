9d7d1bed349303487567048c206055e86dab5616
ilya <marhoily@homrmail.com>
2015-06-20 14:29:22 +0300

    refactor

--- ChessLogic/Board.fs
+++ ChessLogic/Board.fs
@@ -1,8 +1,15 @@
 ﻿module ChessKit.ChessLogic.Board
 
-let IsAttackedBy (side:Color) (position: PositionCore) coordinate = 
-    ScanningExtensions.IsAttackedBy side position (coordinate |> X88.fromCoordinate)    
-let FindKing (color:Color) (position: PositionCore) = 
+let IsAttackedBy (side : Color) (position : PositionCore) coordinate = 
+    ScanningExtensions.IsAttackedBy side position 
+        (coordinate |> X88.fromCoordinate)
+let FindKing (color : Color) (position : PositionCore) = 
     ScanningExtensions.FindKing color position
-let IsInCheck (side:Color) (position: PositionCore) = 
+let IsInCheck (side : Color) (position : PositionCore) = 
     ScanningExtensions.IsInCheck side position
+let ValidateMove (move : Move) (position : Position) = 
+    MoveLegality.Validate move position
+let ValidateLegalMove (move : Move) (position : Position) = 
+    MoveLegality.ValidateLegal move position
+let ParseLegalMove (move : string) (position : Position) = 
+    MoveLegality.ParseLegal move position
