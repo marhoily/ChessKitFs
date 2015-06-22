31a31610ca1eb21e37332d22e8cbda66241f2fde
ilya <marhoily@gmail.com>
2015-06-22 13:21:31 +0300

    remove unnecessary code

--- ChessLogic/CommonUtils.fs
+++ ChessLogic/CommonUtils.fs
@@ -1,16 +1,5 @@
 ﻿namespace ChessKit.ChessLogic
 
-module internal List = 
-    open System.Collections.Generic
-    
-    // Can replace with List.contains in F# 4.0
-    let contains item = List.exists ((=) item)
-    
-    // Can replace with List.except in F# 4.0
-    let except (itemsToExclude : 'T list) list = 
-        let cached = HashSet<'T>(itemsToExclude, HashIdentity.Structural)
-        list |> List.filter cached.Add
-
 module internal Operators = 
     open FParsec
     
